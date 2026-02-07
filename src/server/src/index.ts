0f640c000795876d664906401516e0a098f03c3f
Suraj Chakraborty <surajchakraborty211@gmail.com>
2026-02-07 23:50:28 +0530

    debug

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -49,7 +49,14 @@ const startServer = async () => {
 
     await apolloServer.start();
 
-    app.use(cors());
+    app.use(cors({
+        origin: [
+            process.env.CLIENT_URL || "http://localhost:3000",
+            "http://localhost:3000",
+            "http://localhost:3001"
+        ],
+        credentials: true
+    }));
     app.use(express.json({ limit: "50mb" }));
     app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
